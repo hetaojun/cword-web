cdc57d86d7c817b576f027190c75796f35246028
何涛 <hetao@ciphertrading.com>
2019-06-24 13:58:45 +0800

    init

---
+++ ecosystem.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'WEB',
+      script    : 'index.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      },
+      exec_interpreter: 'babel-node'
+    }
+
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'root',
+      host : '118.190.207.224',
+      ref  : 'origin/master',
+      repo : 'git@gitlab.cyberx.com:hetao/cyberx.git',
+      path : '/data/source/workspace',
+      'post-deploy' : 'git pull && npm install && pm2 startOrRestart ecosystem.config.js --interpreter babel-node -env production'
+    },
+    dev : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/development',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    },
+  }
+};
